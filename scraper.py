21b76c0612598a97da6985d143eefe31a7ac9cac
Kasper Junge <Kasper.Junge@polhus.rootdom.dk>
2020-07-28 08:09:12 +0200

    boligscraper class in separate file

---
+++ scraper.py
@@ -0,0 +1,69 @@
+import json
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+from datetime import date
+
+class BoligScraper(object):
+    
+    r"""Class for scraping www.boligsiden.dk"""
+    
+    def __init__(self, num_listings_per_page=5000):
+        
+        self.num_listings_per_page = num_listings_per_page
+        self.base_url = 'https://www.boligsiden.dk/resultat/1f923c02d4bf4c0ca6b0e7320ee8daee?s=12&sd=false&d=1&p={}&i={}'
+    
+    def scrape(self):
+        
+        print('Scraping..')
+        
+        dfs = []
+        for i in range(10000):
+            
+            print(f'Scraping page {i+1}')
+            # Get url
+            url = self.base_url.format(i, self.num_listings_per_page)
+            df = self._get_listing_page_df(url)
+
+            if df.empty:
+                break
+            else:
+                dfs.append(df)
+        
+        
+        # Concat all dfs
+        print('Concatenating DataFrames..')
+        self.df = pd.concat(dfs)
+        
+        print('Saving scraped data to disk..')
+        self._save_df()
+        
+        print('Scraping finished!')    
+        
+        return self.df
+    
+    def _get_listing_page_df(self, url):
+        
+        # Get all script tags
+        html = requests.get(url).content
+        soup = BeautifulSoup(html, 'html.parser')
+        scripts = soup.find_all('script')
+        
+        # Find script tag with json string
+        for s in scripts:
+            if '__bs_propertylist_result__ ' in str(s):
+                script = str(s)
+
+        # Locate JSON string (find better solution)
+        script = script[45:]
+        script = script[:-16]
+        
+        # Get df
+        data =json.loads(script)['result']['properties'] # convert string to json
+        df = pd.DataFrame(data) # convert to pandas df
+                
+        return df
+        
+    def _save_df(self):
+        self.df.to_pickle(f'./data/boligsiden_{date.today()}.pkl')
+    
