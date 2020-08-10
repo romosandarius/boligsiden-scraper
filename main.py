c455cd49567f1a3450c4e5b6f913b3f5f4792144
Kasper Junge <Kasper.Junge@polhus.rootdom.dk>
2020-08-10 08:44:34 +0200

    added virtual env

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import datetime
+import pandas as pd
 from scraper import BoligScraper
 from integrator import Integrator
+from preprocessor import Preprocessor
 
 #~~~~~~~~~~~~~~#
 #   Pipeline   #
@@ -14,3 +15,13 @@ df = scraper.scrape_listings()
 integrator = Integrator()
 integrator.integrate()
 
+## Load dfs from db
+df_off = pd.read_pickle('./data/database/db_off_market.pkl')
+df_new = pd.read_pickle('./data/database/db_new_on_market.pkl')
+df_on = pd.read_pickle('./data/database/db_on_market.pkl')
+
+# Preprocess dataframes
+preprocessor = Preprocessor()
+df_off = preprocessor.process(df_off)
+df_new = preprocessor.process(df_new)
+df_on = preprocessor.process(df_on)
