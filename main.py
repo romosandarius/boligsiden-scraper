2c7e5ff88b4084d5658239e8b011ed5aad3996a5
Kasper Junge <Kasper.Junge@polhus.rootdom.dk>
2020-08-10 08:20:14 +0200

    cleaned project for notes

--- main.py
+++ main.py
@@ -14,10 +14,3 @@ df = scraper.scrape_listings()
 integrator = Integrator()
 integrator.integrate()
 
-
-
-# TO-DO 
-# - dateLastSeen (instead of dateDetected), should be last df (tjek)
-# - refactor integrator. Add _append_to_or_init_db() method
-# - Include automatic backup of scraping job when ever a job is done
-# - Clean columns (preprocessor)
